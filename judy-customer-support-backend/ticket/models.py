fabc75e7b9222d0da3df26306671fc88aff7cb13
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2024-10-25 18:05:19 +0300

    Created the ticket model and the migration

--- judy-customer-support-backend/ticket/models.py
+++ judy-customer-support-backend/ticket/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Ticket(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_resolved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
