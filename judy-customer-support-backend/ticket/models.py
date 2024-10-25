903246e7bf680d4b26bf2e7a9d333401a1ecebda
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2024-10-25 18:21:21 +0300

    Add the owner and resolved_at fields to the Ticket model

--- judy-customer-support-backend/ticket/models.py
+++ judy-customer-support-backend/ticket/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Ticket(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     is_resolved = models.BooleanField(default=False)
+    owner = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        null=True,
+        default=None, # Added this field (and null=True) after running migration, thus had to specify default value.
+        related_name='tickets'
+    )
     created_at = models.DateTimeField(auto_now_add=True)
+    resolved_at = models.DateTimeField(
+        null=True,
+        default=None
+    )
