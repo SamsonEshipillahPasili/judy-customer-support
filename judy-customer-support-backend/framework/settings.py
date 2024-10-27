988967bbbdc8757c0b500923fd9c4648b9865371
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2024-10-27 13:46:27 +0300

    Add the SIMPLE_JWT config in settings.py

--- judy-customer-support-backend/framework/settings.py
+++ judy-customer-support-backend/framework/settings.py
@@ -132,3 +132,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
 }
+
+# SIMPLE_JWT = {
+#     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+#     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+#     'ROTATE_REFRESH_TOKENS': True,
+#     'BLACKLIST_AFTER_ROTATION': True,
+# }
