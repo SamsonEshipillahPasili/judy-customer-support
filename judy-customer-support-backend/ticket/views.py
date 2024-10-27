db4ea498f687abe56e1e71390b44127d3a5b32ba
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2024-10-27 12:57:49 +0300

    Add the TicketViewSet.On ticket creation, specify the owner of the ticket. Also Ensure the queryset filters tickets that belong to the current user

--- judy-customer-support-backend/ticket/views.py
+++ judy-customer-support-backend/ticket/views.py
@@ -1,10 +1,15 @@
+from django.db.models import QuerySet
 from rest_framework import viewsets
 from .models import Ticket
 from .serializers import TicketSerializer
 
 class TicketViewSet(viewsets.ModelViewSet):
-    queryset = Ticket.objects.all()
     serializer_class = TicketSerializer
+
+    def get_queryset(self) -> QuerySet:
+        user = self.request.user
+        return Ticket.objects.filter(owner=user)
+
 
     def perform_create(self, serializer: TicketSerializer) -> None:
         serializer.save(owner=self.request.user)
