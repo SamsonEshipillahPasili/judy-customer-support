0bd297dd85f07afb0d8ba487efc286d636ef4fe3
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2024-10-27 14:06:11 +0300

    Add the id field as part of the TicketSerializer field

--- judy-customer-support-backend/ticket/serializers.py
+++ judy-customer-support-backend/ticket/serializers.py
@@ -4,5 +4,5 @@ from .models import Ticket
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
-        fields = ['title', 'description', 'is_resolved', 'owner', 'created_at', 'resolved_at']
-        read_only_fields = ['owner', 'created_at', 'resolved_at']
+        fields = ['id','title', 'description', 'is_resolved', 'owner', 'created_at', 'resolved_at']
+        read_only_fields = ['id', 'owner', 'created_at', 'resolved_at']
