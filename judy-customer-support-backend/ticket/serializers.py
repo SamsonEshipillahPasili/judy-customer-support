42e3b79b046ffc894bf91413847c46f94b3f9664
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2024-10-28 13:47:34 +0300

    Restrict the update of a resolved ticket

--- judy-customer-support-backend/ticket/serializers.py
+++ judy-customer-support-backend/ticket/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.validators import ValidationError
+
 from .models import Ticket
 
 class TicketSerializer(serializers.ModelSerializer):
@@ -6,3 +8,10 @@ class TicketSerializer(serializers.ModelSerializer):
         model = Ticket
         fields = ['id','title', 'description', 'is_resolved', 'owner', 'created_at', 'resolved_at']
         read_only_fields = ['id', 'owner', 'created_at', 'resolved_at']
+
+    def validate(self, attrs):
+        if not self.instance:
+            return
+
+        if self.instance.is_resolved:
+            raise ValidationError('You cannot update a resolved ticket')
