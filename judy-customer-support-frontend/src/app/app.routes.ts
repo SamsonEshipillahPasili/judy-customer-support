4e8e0590abb611806bceca524cced23e0402a47b
Eshipillah Pasili Samson <sameshipilla94@gmail.com>
2024-10-29 14:34:12 +0300

    Refine action buttons, add more columns to the list tickets table and adjut font

--- judy-customer-support-frontend/src/app/app.routes.ts
+++ judy-customer-support-frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {LoginComponent} from './login/login.component';
+import {ListTicketsComponent} from './list-tickets/list-tickets.component';
 
 export const routes: Routes = [
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'tickets/list', component: ListTicketsComponent},
 ];
